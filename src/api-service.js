35e78579f933a63a2d8a17f15692f20a74c43d5a
Eremey2410 <2410esv@gmail.com>
2022-08-23 13:20:22 +0300

    all

--- src/api-service.js
+++ src/api-service.js
@@ -1,10 +1,16 @@
-// export default { fetchCountries };
+export default { fetchCountries };
 
-// const BASE_URL = 'https://restcountries.com/v3.1/name';
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 
-// function fetchCountries(name) {
-//   return fetch(`${BASE_URL}/${name}`).then(response =>
-//     // console.log(response.json());
-//     response.json()
-//   );
-// }
+function fetchCountries(name) {
+  return fetch(`${BASE_URL}/${name}`)
+    .then(response =>
+      // console.log(response.json());
+      response.json()
+    )
+    .catch(error => {
+      throw new Error(
+        Notify.failure('Oops, there is no country with that name')
+      );
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import API from './api-service';
 
 // import API from './api-service';
 const DEBOUNCE_DELAY = 300;
@@ -18,144 +19,62 @@ function onInputChange(event) {
   const inputNameCountry = event.target.value.trim();
   console.log(inputNameCountry);
 
-  fetchCountries(inputNameCountry)
-    .then(addMarcup)
+  API.fetchCountries(inputNameCountry)
+    .then(addMarcups)
     .catch(error => {
-      console.log('beda', error);
+      console.log(error);
       Notify.failure('Oops, there is no country with that name');
     });
 }
-
-// запрос на сервер
-function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    console.log(response);
-    return response.json();
-  });
+// очистка разметки
+function clearMarkup() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
-
 // добавление html разметки
-function addMarcup(country) {
+function addMarcups(country) {
   const properties = country;
+
+  clearMarkup();
 
   console.log(country.length);
   if (country.length > 10) {
     Notify.info('Too many matches found. Please enter a more specific name.');
-  }
-  if (country.length >= 2 && country.length <= 10) {
-    const markupFlags = properties.map(
-      property =>
-        ` <img src="${property.flags.svg}" alt="${property.name.common}" />
-      ${property.name.common}`
-    );
-    refs.countryInfo.innerHTML = markupFlags;
-  }
-  if (country.length === 1) {
-    const markup = properties
-      .map(
-        property =>
-          `<li class="country-item"> <img src="${property.flags.svg}" alt="${
-            property.name.common
-          }" />
-      ${property.name.common}</li>
-          <li class="country-item"> <p class="country-text">Capital:<span class= "country-info-description"> ${
-            property.capital
-          }</span></p></li>
-          <li class="country-item"> <p class="country-text">Population:<span class= "country-info-description"> ${
-            property.population
-          }</span></p></li>
-          <li class="country-item"> <p class="country-text">Languages:<span class= "country-info-description"> ${Object.values(
-            property.languages
-          )}</span></p></li>`
-      )
-      .join('');
-    refs.countryList.innerHTML = markup;
+  } else if (country.length >= 2 && country.length <= 10) {
+    addMarkupInfo(properties);
+  } else {
+    addMarkupList(properties);
   }
 }
+function addMarkupList(properties) {
+  const markup = properties
+    .map(
+      property =>
+        `<li class="country-item"> <img src="${property.flags.svg}" alt="${
+          property.name.common
+        }" />
+      <span class="list-description"> ${property.name.common}</span></li>
+          <li class="country-item"> <p class="country-text">Capital:<span class= "country-list-description"> ${
+            property.capital
+          }</span></p></li>
+          <li class="country-item"> <p class="country-text">Population:<span class= "country-list-description"> ${
+            property.population
+          }</span></p></li>
+          <li class="country-item"> <p class="country-text">Languages:<span class= "country-list-description"> ${Object.values(
+            property.languages
+          )}</span></p></li>`
+    )
+    .join('');
+  refs.countryList.innerHTML = markup;
+}
 
-// import './css/styles.css';
-// import debounce from 'lodash.debounce';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// // import API from './api-service';
-// const DEBOUNCE_DELAY = 300;
-
-// const refs = {
-//   input: document.querySelector('#search-box'),
-//   countryList: document.querySelector('.country-list'),
-//   countryInfo: document.querySelector('.country-info'),
-// };
-
-// refs.input.addEventListener('input', debounce(onInputChange, 300));
-// let inputNameCountry = '';
-
-// // при вводе значения в инпут вызывается функция запроса на сервер
-// function onInputChange(event) {
-//   inputNameCountry = event.target.value.trim();
-//   console.log(inputNameCountry.length);
-
-//   fetchCountries(inputNameCountry)
-//     .then(addMarcup)
-//     .catch(error => {
-//       Notify.failure('Oops, there is no country with that name');
-//     });
-// }
-
-// // запрос на сервер
-// function fetchCountries(name) {
-//   return fetch(
-//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-//   ).then(response => {
-//     console.log(response);
-//     return response.json();
-//   });
-// }
-
-// // добавление html разметки
-// function addMarcup(countries) {
-//   // const properties = country;
-
-//   console.log(countries.length);
-//   if (countries.length > 10) {
-//     Notify.info('Too many matches found. Please enter a more specific name.');
-//   }
-//   if (countries.length >= 2 && countries.length <= 10) {
-//     refs.countryList.insertAdjacentHTML(
-//       'beforeend',
-//       createListItems(countries)
-//     );
-//   }
-//   if (countries.length === 1) {
-//     refs.countryInfo.insertAdjacentHTML('beforeend', createInfo(countries));
-//   }
-// }
-
-// function createListItems(countries) {
-//   return countries
-//     .map(
-//       country =>
-//         `<li class="country-item"><img src="${country.flags.svg}" alt="${
-//           country.name.common
-//         }"/> <span class="country-name">${country.name.common}</span></li>
-//           <li class="country-item"> <p class="country-text">Capital:<span class= "country-info-description"> ${
-//             country.capital
-//           }</span></p></li>
-//           <li class="country-item"> <p class="country-text">Population:<span class= "country-info-description"> ${
-//             country.population
-//           }</span></p></li>
-//           <li class="country-item"> <p class="country-text">Languages:<span class= "country-info-description"> ${Object.values(
-//             country.languages
-//           )}</span></p></li>`
-//     )
-//     .join('');
-// }
-// function createInfo() {
-//   return `<ul class="country-info-list">
-//   <li class="country-info-item"><img src="${country.flags.svg}" alt="${country.name.common}"/> <span class="country-name">${country.name.common}</span></li>
-//         <li class="country-info-item"><p class="country-text">Capital:<span class= "country-info-description"> ${country.capital}</span></p></li>
-//         <li class="country-info-item"><p class="country-text">Capital:<span class= "country-info-description"> ${country.capital}</span></p></li>
-//         <li class="country-info-item"><p class="country-text">Capital:<span class= "country-info-description"> ${country.capital}</span></p></li>
-// </ul>`;
-// }
+function addMarkupInfo(properties) {
+  const markupFlags = properties
+    .map(
+      property =>
+        `<div class="country-info-one"><img src="${property.flags.svg}" alt="${property.name.common}" />
+     <span class="country-info-one-description"> ${property.name.common}</span></div>`
+    )
+    .join('');
+  refs.countryInfo.innerHTML = markupFlags;
+}
